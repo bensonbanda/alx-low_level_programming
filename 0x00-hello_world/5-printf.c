f4ceb3473be3555768ca0ef70337ae126a2614a7
bensonbanda <bbanda79@yahoo.com>
2022-09-08 16:35:04 -0700

    add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entrly point
+ * @void: will not have any parameter
+ * Description: print some text to the screen
+ * Return: none
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
