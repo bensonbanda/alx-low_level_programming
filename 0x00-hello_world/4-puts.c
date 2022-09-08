f063a2548cbeeba943732710be7501bf5227fec1
bensonbanda <bbanda79@yahoo.com>
2022-09-08 15:40:28 -0700

    Add 4-put.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * The following will print some text to the 
+ * screen and return 0 value
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
