4298f86a35cc477e0e5d85e861a95f726932e8fd
bensonbanda <bbanda79@yahoo.com>
2022-09-08 15:49:41 -0700

    Add 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 /**
- * The following will print some text to the 
- * screen and return 0 value
+ * function_name: main, entry point
+ * @parametervoid: function will not have parameters
+ (* 
+ * Description: The function is the main entrly point for the program.)? 
+ (*sectin_header: main function)*
+ * Return: a function will not return any value. 
  */
 int main(void)
 {
