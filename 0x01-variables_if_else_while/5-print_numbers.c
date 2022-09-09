9a9c2941ad7efe937302411e0a01678ddf63740a
bensonbanda <bbanda79@yahoo.com>
2022-09-09 06:51:45 -0700

    Add 5 code

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entrly point
+ * @void: zero parameter
+ * Description: will print numbers to screen
+ * Return: zero value
+ */
+
+int main(void)
+{
+	char numbers[] = "0123456789";
+	
+	puts(numbers);
+
+	return (0);
+}
