06f6f3cafb9a6ef55f6d9259a68967ee0b79494f
bensonbanda <bbanda79@yahoo.com>
2022-09-09 04:28:45 -0700

    Add 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entrly point
+ * @void: zero parameter
+ * Description: will print alphabet to the screen
+ * Return: Zero value.
+ */
+
+int main(void)
+{
+	char alpha[26] = "abcdefghijklmnopqrstuvxyz";
+	char alpha2[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	puts(alpha);
+	puts(alpha2);
+
+	return (0);
+}
+
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 0-positive_or_negative.c will print out if number is postive or negative.
 
+3-print_alphabets.c will print alphabet 
