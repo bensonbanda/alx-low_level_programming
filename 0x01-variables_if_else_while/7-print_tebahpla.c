f6c1d0b3e55a267cce22a52831d314b2daea726a
bensonbanda <bbanda79@yahoo.com>
2022-09-09 07:17:01 -0700

    Add 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - entrly point
+ * @void: none
+ * Description: will print alphabet in reverse
+ * Return: zero
+ */
+
 int main(void)
 {
 	char alpha[] = "zyxwvutsrqponmlkjihgfedcba";
