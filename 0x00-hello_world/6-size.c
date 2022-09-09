9bf3c7782956ad66af8a96271f1309f4896a4bc9
bensonbanda <bbanda79@yahoo.com>
2022-09-08 18:44:10 -0700

    Add source file 7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,7 @@
 
 int main(void)
 {
+	size
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(int));
