c9d6f7213fb82b490f146402c7c94d9bdc2ade2d
bensonbanda <bbanda79@yahoo.com>
2022-09-09 03:46:58 -0700

    Add 0 sutce file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * Main - Entrly point
+ * @void: no parameters
+ * Description: THe function will print negative and postive intergers.
+ * Return: zero value will be returned.
+ */
+
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}else{
+		printf("%d is postive\n", n);
+	}
+}
+
