5322314c64b084234a5585f00def0db9167c9e9c
bensonbanda <bbanda79@yahoo.com>
2022-09-08 17:57:30 -0700

    Add 101-qoute.c source file

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entrly point
+ * @void: no parameters specefied
+ * Description: the function will print some text.
+ * Return: the value of one
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
