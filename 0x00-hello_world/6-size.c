e0918ad65023fa7e0610024147913cdb40bfe8d4
bensonbanda <bbanda79@yahoo.com>
2022-09-08 18:46:17 -0700

    Add source file 7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long long: %zu byte(s)\n", sizeof(long long));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
-
+	printf(stderr, "Anything");
 	return (0);
 }
