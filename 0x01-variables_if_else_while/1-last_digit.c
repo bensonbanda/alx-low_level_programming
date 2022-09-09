a65ed606a23f655d917437ea7b67776469b673e0
bensonbanda <bbanda79@yahoo.com>
2022-09-09 05:35:41 -0700

    Add source one

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>                                                                                                                    
+#include <time.h>
+/* more headers goes there */ 
+/* betty style doc for function main goes there */ 
+/**
+ * main - entrly point
+ * @void: no parameter
+ * Description: print end od the line to the screen.
+ * Return: zero value.
+ */
+int main(void){
+
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int ldigit = n % 10;
+
+	switch(n)
+	{
+	case(n > 5):
+		printf("The last digit of %d is %d and is greater than 5", n,ldigit);
+		break;
+	case(n == 0):
+		printf("The last digit of %d is %d and is 0", n,ldigit);
+		break;
+	case(n < && n != 0):
+		printf("The last digit of %d is %d and is 0", n,ldigit);
+		break;
+	}
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,4 +1,4 @@
 0-positive_or_negative.c will print out if number is postive or negative.
-
+1-last_digit.c script will print end of the line.
 2-print_alphabet.c will print alphabet in lower case
 3-print_alphabets.c will print alphabet 
