70e5ec8929da3b51adec75cc02f9660ac3df4811
bensonbanda <bbanda79@yahoo.com>
2022-09-08 18:57:14 -0700

    Add source file 7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,11 @@
 
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long long: %zu byte(s)\n", sizeof(long long));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
-	printf(stderr, "Anything");
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of a long long: %lu byte(s)\n", (unsigned long)sizeof(long long));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
+	
 	return (0);
 }
