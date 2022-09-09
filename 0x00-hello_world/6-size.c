8a09b4156d928097d4fdc0624ed4987122ce3939
bensonbanda <bbanda79@yahoo.com>
2022-09-08 17:28:39 -0700

    add source file 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,13 +10,11 @@
 
 int main(void)
 {
-	int interger = sizeof(int);
-	int chara = sizeof(char);
-	printf("Size of a char: %d byte(s)\n", chara);
-	printf("Size of an int: %d byte(s)\n", interger);
-	printf("Size of a long int: %d byte(s)\n", sizeof(long));
-	printf("Size of a long long: %d byte(s)\n", sizeof(long long));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long));
+	printf("Size of a long long: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
 }
