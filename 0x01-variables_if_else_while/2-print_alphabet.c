11904f83d63ea0e536e77f5ac66b8a610a05af1c
bensonbanda <bbanda79@yahoo.com>
2022-09-09 04:40:15 -0700

    add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * Main - Entrly point
+ * @void: zerop parameter
+ * Description: function will print alphabetic letters
+ * Return: zero value
+ */
+
+int main(void)
+{
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	
+	puts(alpha);
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 0-positive_or_negative.c will print out if number is postive or negative.
 
+2-print_alphabet.c will print alphabet in lower case
 3-print_alphabets.c will print alphabet 
