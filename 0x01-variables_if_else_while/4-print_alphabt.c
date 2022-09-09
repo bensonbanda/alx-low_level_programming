bcf32e09277b8266101cb026eeeef6c7204c07a7
bensonbanda <bbanda79@yahoo.com>
2022-09-09 06:42:14 -0700

    try

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entrly point
+ * @void: no prameters
+ * Description: will print alphabet except e and q
+ * Return: zero
+ */
+
+int main(void)
+{
+	char alpha[] = "abceddddd";
+	putchar(alpha);
+
+	return (0);
+}
+
