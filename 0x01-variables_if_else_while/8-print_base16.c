eea963f5e1d56f064be3efcc628539bbbd41e7ae
bensonbanda <bbanda79@yahoo.com>
2022-09-09 07:21:59 -0700

    Add 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>>
+
+/**
+ * main - entrly point
+ * @void: zero parameter
+ * Description: hex value will be printed
+ * Return: zero value
+ */
+int main(void)
+{
+	char hexv[] = "0123456789abcdef";
+
+	puts(hexv);
+
+	return (0);
+}
