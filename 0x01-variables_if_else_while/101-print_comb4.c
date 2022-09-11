6fde94cde1c12bf3164fb97c4132cc5481791229
bensonbanda <bbanda79@yahoo.com>
2022-09-11 02:32:09 -0700

    Add 101 source file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - entrly point
+ * @void: entrly point
+ * Description: will print combination of int numbers to the screen
+ * Return: zero value
+ */
+
+int main(void)
+{
+	int number1, number2;
+
+	for (number1 = 0; number1 <= 98; number1++)
+	{
+		for (number2 = number1 + 1; number2 <= 99; number2++)
+		{
+			putchar((number1 / 10) + '0');
+			putchar((number1 % 10) + '0');
+			putchar(' ');
+			putchar((number2 / 10) + '0');
+			putchar((number2 % 10) + '0');
+
+			if (number1 == 98 && number2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
