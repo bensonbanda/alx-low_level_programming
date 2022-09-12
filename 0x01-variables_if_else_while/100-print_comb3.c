268199ed9f45ebc30070803b015876da3e936e31
bensonbanda <bbanda79@yahoo.com>
2022-09-12 15:38:02 -0700

    Add 100 source file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**                                                                                                                                     
+ *  * main - entrly point                                                                                                                  
+ *   * @void: no parameters                                                                                                                 
+ *    * Description: will print decimal numbers from 0 -9                                                                                    
+ *     * Return: zero value                                                                                                                   
+ *      */       
+
+/**
+ * main - entrly point
+ * @void: no parameters
+ * Description: the function will display combination of numbers
+ * Return: Zero Value
+ */
+
+int main(void)
+{
+	int number1,number2;
+
+	for (number1 = 0; number1 < 9; number1++)
+	{
+		for (number2 = number1 + 1; number2 < 10; number2++)
+		{
+			putchar((number1 % 10) + '0');
+			putchar((number1 % 10) + '0');
+
+			if (number1 == 8 && number2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
