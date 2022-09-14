bca7984a10b5ce87a0ecd85b5cd18d10ee4f3125
bensonbanda <bbanda79@yahoo.com>
2022-09-14 03:53:45 -0700

    add 0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * Main - entrly point
+ * @void: no parameters
+ * Descripttion: the function will display putchar to the screen
+ * Return: zero value
+ */
+
+int main(void)
+{
+	
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+	
+	puts("\n");
+
+	return (0);
+}	
