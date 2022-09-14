65654b655374d1fc9ee6663d2a2040d673e8eb2b
bensonbanda <bbanda79@yahoo.com>
2022-09-14 14:21:01 -0700

    Add 3 source file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_islower - will return 1 or 0
+ *@int c: int parameter
+ *Return: int
+ */
+
+int _islower(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (0);
+	else if (c >= 97 && c <= 122)
+		return (1);
+}
+
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,5 @@
 0-putchar.c will print out putchar to the screen
 1-alphabet.c wil display alphabet characters in small letters
 
+3-islower.c will return 1 if character is lowercase otherwise will return
+
