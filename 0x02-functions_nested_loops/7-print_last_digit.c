d8e537291609bc7e4fee12b58afa6d84f7c909b8
bensonbanda <bbanda79@yahoo.com>
2022-09-14 15:40:54 -0700

    Add 7 source file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *print_last_digit - will print last digit in a number
+ *@d: interger parameter
+ *Return: interger value
+ */
+
+int print_last_digit(int d)
+{
+	d %= 10
+	return (d);
+}
