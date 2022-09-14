3276549e8e66c6051c8bd1e74d5074e2541aa164
bensonbanda <bbanda79@yahoo.com>
2022-09-14 04:47:03 -0700

    Add source file 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,31 +4,31 @@ void print_alphabet(void);
 
 
 /**
- * Main - entrly point
+ * print_alphbet characters
  * @void: no parameters
  * Description: the function will display the results from the other fun
+ * Return: zero value
+ */
+
+void print_alphabet(void)
+{
+	int number = 0;
+
+	for (number = 97; number <= 122; number++)
+		putchar(number)
+}
+
+
+/**
+ * Main - Entrly point 
+ * @void: no parameters
+ * Description: the function will display alphabet characters
  * Return: zero value
  */
 
 int main(void)
 {
 	print_alphabet();
+
 	return (0);
-}
-
-/**
- * print alphabet 
- * @void: no parameters
- * Description: the function will display alphabet characters
- * Return: zero value
- */
-void print_alphabet(void)
-{
-	int number = 0;
-
-	for (number = 97; number <= 122; number++)
-	{
-		putchar(number);
-	}
-
 }
