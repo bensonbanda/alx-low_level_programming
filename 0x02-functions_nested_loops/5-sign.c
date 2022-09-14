389ac5278976be6f70e3266152c07e47d02cdecd
bensonbanda <bbanda79@yahoo.com>
2022-09-14 14:46:32 -0700

    Add 5 source file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *print_sign - display +,- or 0
+ *@n: interger parameter
+ *Return: 0,1 or -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+
+	else if (n < 0)
+	{
+		putchar(45);
+		return (-1);
+	}
+
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -3,3 +3,5 @@
 
 3-islower.c will return 1 if character is lowercase otherwise will return
 4-isalpha.c will display if a characeter isletter or not.
+5-sign.c will display and return postive, negative or 0.
+
