e751a193c92ca2408cb26d29debc7db20781ee79
bensonbanda <bbanda79@yahoo.com>
2022-09-14 15:09:42 -0700

    add 6 source file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ *_abs - display absolute values
+ *@v: interger parameter
+ *Return: interger value
+ */
+
+int _abs(int v)
+{
+	if (v < 0)
+	{
+		v *= -1;
+
+		_putchar(v);
+	}
+
+	else
+		putchar(v);
+	
+	return (0);
+}
