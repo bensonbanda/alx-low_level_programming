77b1c29f86cf243a16307443e713037d16078d29
bensonbanda <bbanda79@yahoo.com>
2022-09-15 16:22:10 -0700

    add 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - the code will print numbers except two and four
+ * @void: no parameters
+ * Return: NOo value
+ */
+
+
+void print_most_numbers(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		if (digit == 2 || digit == 4)
+			continue;
+		
+		_putchar(digit);
+		digit++;
+	}
+	
+}
