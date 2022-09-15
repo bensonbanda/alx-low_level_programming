9a6d37992e5e0ee4822d95f7bc8593e58e5f2696
bensonbanda <bbanda79@yahoo.com>
2022-09-15 16:48:21 -0700

    add 5 file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - will print more numbers
+ * @void: no parameters
+ * Return: no value
+ */
+
+void more_numbers(void)
+{
+	int number, number2;
+
+	for (number = 0; number <= 9; number++)
+	{
+		for (number2 = 0; number2 <= 14; number2++)
+		{
+			if (number2 > 9)
+				_putchar((number2 / 10) + '0');
+			_putchar((number2 % 10) + '0');
+		}
+
+	}
+	_putchar('\n');
+}
+
