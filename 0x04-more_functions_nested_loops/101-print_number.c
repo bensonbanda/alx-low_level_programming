650af786c3b7d01a3da4658d7f09a9fa712d1638
bensonbanda <bbanda79@yahoo.com>
2022-09-15 23:06:03 -0700

    add 101 source file

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - the function will interger numbers
+ * @n: interger parameter
+ * Return: No value
+ */
+
+void print_number(int n)
+{
+	unsigned int number = n;
+
+	if (n < 0)
+	{
+		_putchar('_');
+		number = -number;
+	}
+
+	if ((number / 10) > 0);
+		print_number(number / 10);
+
+	_putchar((number % 10) > 0);
+}
