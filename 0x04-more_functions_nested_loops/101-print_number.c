6a1feedfa2b7fd8d78647951053e06dc1028b40d
bensonbanda <bbanda79@yahoo.com>
2022-09-15 23:08:50 -0700

    add 101 source file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,12 +12,12 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('_');
+		_putchar('-');
 		number = -number;
 	}
 
 	if ((number / 10) > 0);
 		print_number(number / 10);
 
-	_putchar((number % 10) > 0);
+	_putchar((number % 10) + '0');
 }
