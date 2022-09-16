f88868799fb26012988cc416415f4a7c83874270
bensonbanda <bbanda79@yahoo.com>
2022-09-15 17:15:10 -0700

    add 7 file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - will print diagonal characters
+ * @n: interger parameters
+ * Return: no value
+ */
+
+void print_diagonal(int n)
+{
+	int num11, num2;
+
+	if (n > 0)
+	{
+		for (num1 = 0; num1 < n; num1++)
+		{
+			for (num2 = 0; num2 < num1; num2++)
+				_putchar(' ');
+			_putchar('\\');
+
+			if (num1 == n -1)
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
