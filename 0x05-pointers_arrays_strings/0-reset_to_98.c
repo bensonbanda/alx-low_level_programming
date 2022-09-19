b508ca20882e46848471da8edd5a9549cad5cede
bensonbanda <bbanda79@yahoo.com>
2022-09-19 02:58:12 -0700

    Add files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - will reset variable value inside the function
+ *@*n: interger pointer 
+ *Return:No value 
+ */
+
+void reset_to_98(int *n)
+{
+	int *m;
+	
+	*m = 98;
+}
+
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0-reset_to_98.c will change the value of a variable using pointer inside function
