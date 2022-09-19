ddf03cea4b421b266058827d380a006546da4e46
bensonbanda <bbanda79@yahoo.com>
2022-09-19 03:10:48 -0700

    add files

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 
 /**
  *reset_to_98 - will reset variable value inside the function
- *@n: interger pointer 
- *Return: no value 
+ *@n: interger pointer
+ *Return: no value
  */
 
 void reset_to_98(int *n)
