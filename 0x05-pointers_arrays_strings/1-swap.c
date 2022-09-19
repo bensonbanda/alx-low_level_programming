11166ff6e2eaec82050fa97d9ae3dad84088ce44
bensonbanda <bbanda79@yahoo.com>
2022-09-19 03:19:23 -0700

    add file2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - the function will swap variables values 
+ * @a: interger pointer
+ * @b: interger pointer
+ * Return: no value
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
