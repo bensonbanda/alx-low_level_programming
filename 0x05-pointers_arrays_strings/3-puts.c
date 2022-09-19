bdaa391013b247ae46e97dab831c411687365d16
bensonbanda <bbanda79@yahoo.com>
2022-09-19 04:34:43 -0700

    add file3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - will display strings 
+ * @str: character parameter
+ * Return: no value
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+	
+	_putchar('\n');
+}
