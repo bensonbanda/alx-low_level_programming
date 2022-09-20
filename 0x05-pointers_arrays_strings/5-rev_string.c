706dc2db2b443d46d932c924172621072674f119
bensonbanda <bbanda79@yahoo.com>
2022-09-20 14:46:06 -0700

    add 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - the function will reverse the string
+ * @s: charcter pointer parameter
+ * Return: no value
+ */
+
+void rev_string(char *s)
+{
+	int number = 10;
+
+	while (number >= 0)
+	{
+		char a = *(s + number);
+		
+		_putchar(a);
+		number--;
+	}
+	_putchar('\n');
+}
