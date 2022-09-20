4244326c76fd0f72be154ebe9fa806378d233dae
bensonbanda <bbanda79@yahoo.com>
2022-09-20 15:52:31 -0700

    ad 8

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * _strcpy - the function will copy string 
+ *@dest: parameter
+ *@src: parameter
+ *Return: character value
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int num = 0;
+
+	while ((*src + inc) != '\0')
+	{
+		*(dest + num) = *(src + num);
+		num++;
+	}
+	*(dest + num) = '\0';
+
+	return (dest);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,4 +10,6 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void rev_string(char *s);
+char *_strcpy(char *dest, char *src);
+
 #endif /* MAIN_H */
