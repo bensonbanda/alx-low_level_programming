934981771b07ec9409df795431bea00d658182fa
bensonbanda <bbanda79@yahoo.com>
2022-09-20 17:32:51 -0700

    ad 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.c"
+
+
+/**
+ * _atoi - change string to int
+ * @s: parameter
+ * Return: the interger value
+ */
+
+int _atoi(char *s)
+{
+	int num = 1;
+
+	unsigned int num1 = 0;
+
+	do
+	{
+		if (*s == '-')
+			num *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num1 *= 10 + (*s - '0');
+		else if (num1 > 0)
+			break;
+	}while (*s++)
+	{
+		return (num1 *num);
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -15,5 +15,6 @@ char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
 void puts_half(char *str);
 void puts2(char *str);
+int _atoi(char *s);
 
 #endif /* MAIN_H */
