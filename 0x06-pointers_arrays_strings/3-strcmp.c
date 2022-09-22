b349b959b1ae3faad68b6f21f14a129fbc2d7b25
bensonbanda <bbanda79@yahoo.com>
2022-09-22 00:05:56 -0700

    add file 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+
+/**
+ *_strcmp - the function will be comparing thow strrings
+ *@s1: character parameter
+ *@s2: charcter parameter
+ *
+ *
+ *Return: interger value
+ *
+ *
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int num = 0, num = 0;
+
+	while (num)
+	{
+		if (s1[num] == '\0' && s2[num] == '\0')
+			break;
+
+		else if (s1[num] == '\0')
+		{
+			num2 = s1[num];
+			break;
+		}
+		else if (s2[num] == '\0')
+		{
+			num2 = s1[num];
+			break;
+		}
+		else if (s1[num] != s2[num])
+		{
+			num2 = s1[num] - s2[num];
+			break;
+		}
+		else
+			num++;
+	}
+
+	return (num2);
+}
