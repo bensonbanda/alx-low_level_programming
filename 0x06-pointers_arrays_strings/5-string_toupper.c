1314afb0879ff6270ee47d01fd931c854ef1358b
bensonbanda <bbanda79@yahoo.com>
2022-09-22 00:28:15 -0700

    add file 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *string_toupper - will change small letter to uppercase letters
+ *@st: character parameter
+ *Return: character value
+ */
+
+char *string_toupper(char *st)
+{
+	int num = 0;
+	
+	while (st[nm++])
+	{
+		f (st[nm] >= 'a' && st[nm] <= 'z')
+			st[nm] -= 32;
+	}
+
+	return (st);
+}
