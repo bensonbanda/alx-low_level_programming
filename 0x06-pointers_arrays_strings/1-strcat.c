165a5397272281d687696a61bb85d01c6546c2bf
bensonbanda <bbanda79@yahoo.com>
2022-09-21 23:35:11 -0700

    add files

---
+++ 0x06-pointers_arrays_strings/1-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strcat - wil join two or more words
+ *@dest: parameter
+ *@src: parameter
+ *@n: parameter
+ *Return: character value
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int num = 0, num2 = 0;
+
+	while (dest[num++])
+		num2++;
+
+	for (num = 0; src[num] && num < n; num++)
+		dest[num2++] = src[num];
+
+	return (dest);
+}
