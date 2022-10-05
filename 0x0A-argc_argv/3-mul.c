cd33d597d632292064c278591c8957f42b1de9d1
bensonbanda <bbanda79@yahoo.com>
2022-10-05 07:04:00 -0700

    add 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - the program will display the product of two numbers
+ *
+ * @argc: parameter
+ * @argv: parameter
+ * Return: zero value
+ */
+
+int main (int argc, char *argv[])
+{
+	int i, n, answer;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	i = atoi(argv[1]);
+	n = atoi(argv[2]);
+
+	answer = i * n;
+
+	printf("%d\n", answer);
+
+	return (0);
+}
