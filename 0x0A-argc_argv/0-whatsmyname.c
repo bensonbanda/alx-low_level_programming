bb80f6a6d77650866c0f0bd96e7f4686183b9792
bensonbanda <bbanda79@yahoo.com>
2022-10-05 06:41:13 -0700

    Add 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,9 @@
  *Return: 0 value
  */
 
-int main(int __attribute__((__unused__)) argc, char* argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	printf("%s", argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
