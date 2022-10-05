9a242929da3014625709750b93018f46743719f2
bensonbanda <bbanda79@yahoo.com>
2022-10-05 06:33:31 -0700

    Add 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - the function will print the name of the program
+ *
+ *@argc: parameter
+ *@argv: array parameter of strings
+ *Return: 0 value
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s", argv[0]);
+}
