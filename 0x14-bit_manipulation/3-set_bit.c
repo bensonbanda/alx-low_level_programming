65d7768e5f8f78e086b2db706f8daf51ddf4ad73
bensonbanda <bbanda79@yahoo.com>
2022-10-27 15:19:25 -0700

    add 4

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * set_bit - sets a value of a bit to 1 at 
+ * a given index.
+ * @n: unsigned long int pointer.
+ * @index: index of a bit.
+ *
+ *
+ * Return: 1 or -1.
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int num;
+
+	if (index > 63)
+		return (-1);
+
+	num = 1 << index;
+	*n = (*n | num);
+
+
+	return (-1);
+
+}
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -1,4 +1,5 @@
 0-binary_to_uint.c the program converts binary numbers to unsigned interger.
 1-print_binary.c the program will prints the binary representation of a number.
 2-get_bit.c the program returns a bit at a given index.
+3-set_bit.c the program sets a value of a bit to 1 at agiven index.
 
